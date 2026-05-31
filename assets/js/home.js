0528141bfc0ca2f1472f827ef64299c2cdf13a21
MatheusHenriqueOtto <matheusotto1906@gmail.com>
2026-05-31 11:25:17 -0300

    Adiciona a pagina home mais login

---
+++ assets/js/home.js
@@ -0,0 +1,5 @@
+function loginRealizado(){
+    const divLogin = document.getElementById("div-login");
+    divLogin.classList.add("hidden");
+    alert("login realizado, pode acessar as funcionalidades")
+}
